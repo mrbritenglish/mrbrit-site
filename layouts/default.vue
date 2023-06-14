29ee2158f0e630cf1115fa6cd1279edaf5acc4c2
Thais Martins <thaismartins1999@gmail.com>
2023-06-13 22:26:45 -0300

    fix: convert illustration at component

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,9 +3,11 @@
     <header
       class="bg-primary pb-4 h-96 relative z-10 shadow-[0px_10px_8px_-4px_#718096]"
     >
-      <nav class="p-4">
+      <nav class="p-4 h-28">
         <img
+          style="width: 100px; height: auto"
           width="100"
+          height="100"
           src="~/assets/images/logo-brit.png"
           alt="Logo MrBrit"
         />
@@ -19,11 +21,7 @@
           >
             Aprenda Inglês do seu jeito
           </h1>
-          <img
-            class="w-48 md:w-96"
-            src="~/assets/images/learning.svg"
-            alt="Ilustração"
-          />
+          <Learning />
         </div>
       </div>
     </header>
